209b6664afb76381875feef32893267efa1deeda
Brian <dpi.studio@gmail.com>
2025-08-18 20:52:28 +0800

    feat: 新增 Bootstrap 依賴與樣式
    - 在 package.json 中新增 Bootstrap 依賴
    - 在 package-lock.json 中更新 Bootstrap 相關資訊
    - 在 main.js 中引入 Bootstrap 的 CSS 和 JS

--- src/main.js
+++ src/main.js
@@ -2,6 +2,9 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
+
 const app = createApp(App)
 
 app.use(createPinia())
